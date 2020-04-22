9915df4b4710dc171b3c23875cb93f85d65b90dc
liuyongli <bingjinb@gmail.com>
2020-04-22 17:35:15 +0800

    添加：注册中心Eurake和Spring Boot Admin

---
+++ registration-center/src/main/java/com/bugod/EurakeApplication.java
@@ -0,0 +1,9 @@
+package com.bugod;
+
+//@SpringBootApplication
+public class EurakeApplication {
+//    public static void main(String[] args) {
+//        SpringApplication.run(DemoApplication.class, args);
+//    }
+
+}
