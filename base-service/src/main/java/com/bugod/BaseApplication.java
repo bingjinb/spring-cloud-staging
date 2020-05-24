997e1b48655a5e497bb83d3aa7a890e27b245eed
liuyongli <bingjinb@gmail.com>
2020-05-25 01:08:17 +0800

    添加：启用 Eureka 注册中心，注册 base-servie 到注册中心，Eureka和base-servie分别引入spring boot admin监控

--- base-service/src/main/java/com/bugod/BaseApplication.java
+++ base-service/src/main/java/com/bugod/BaseApplication.java
@@ -2,8 +2,10 @@ package com.bugod;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableDiscoveryClient
 @EnableAsync
 @SpringBootApplication(scanBasePackages = {"com.bugod"})
 public class BaseApplication {
--- registration-center/src/main/java/com/bugod/EurakeApplication.java
+++ registration-center/src/main/java/com/bugod/EurakeApplication.java
@@ -1,9 +1,14 @@
 package com.bugod;
 
-//@SpringBootApplication
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
 public class EurakeApplication {
-//    public static void main(String[] args) {
-//        SpringApplication.run(DemoApplication.class, args);
-//    }
+    public static void main(String[] args) {
+        SpringApplication.run(EurakeApplication.class, args);
+    }
 
 }
